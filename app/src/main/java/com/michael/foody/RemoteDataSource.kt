475285861b5bce31b37d5a52dc58a224b71fb54b
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-12-22 15:20:15 -0600

    Inject RemoteDataSource class

--- app/src/main/java/com/michael/foody/RemoteDataSource.kt
+++ app/src/main/java/com/michael/foody/RemoteDataSource.kt
@@ -1,4 +1,14 @@
 package com.michael.foody
 
-class RemoteDataSource() {
+import com.michael.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend  fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipesApi.getRecipes(queries)
+    }
 }
