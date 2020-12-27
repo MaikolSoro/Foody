7a1dcf07aeb624ffbb4f03c7aa22b61ea451596e
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-12-27 17:43:46 -0600

    Design RecipesBottomSheet

---
+++ app/src/main/java/com/michael/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -0,0 +1,22 @@
+package com.michael.foody.ui.fragments.recipes.bottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.michael.foody.R
+
+
+class RecipesBottomSheet : BottomSheetDialogFragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+    }
+
+}
