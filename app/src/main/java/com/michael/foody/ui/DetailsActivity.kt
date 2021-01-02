f967fb8f938c1a90c599232049881dac47dd883b
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-01-01 19:52:41 -0600

    Design activity_details Layout

---
+++ app/src/main/java/com/michael/foody/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.michael.foody.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.michael.foody.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
