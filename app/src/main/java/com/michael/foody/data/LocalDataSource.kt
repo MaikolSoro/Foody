125db63ea5a3f8af5d833115ba6afda176744b97
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-01-05 16:04:21 -0600

    Room Database Favorite Recipes

--- app/src/main/java/com/michael/foody/data/LocalDataSource.kt
+++ app/src/main/java/com/michael/foody/data/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.michael.foody.data
 
 import com.michael.foody.data.database.RecipesDao
+import com.michael.foody.data.database.entities.FavoritesEntity
 import com.michael.foody.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,11 +10,27 @@ class LocalDataSource @Inject constructor(
     private val recipesDao: RecipesDao
 ) {
 
-    fun readDataBase(): Flow<List<RecipesEntity>> {
+    fun readRecipes(): Flow<List<RecipesEntity>> {
         return recipesDao.readRecipes()
+    }
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>> {
+        return recipesDao.readFavoriteRecipes()
     }
 
     suspend fun insertRecipes(recipesEntity: RecipesEntity) {
         recipesDao.insertRecipes(recipesEntity)
     }
+
+    suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity){
+        recipesDao.insertFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity){
+        recipesDao.deleteFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun deleteAllFavoriteRecipes(){
+        recipesDao.deleteAllFavoritesRecipes()
+    }
+
 }
--- app/src/main/java/com/michael/foody/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/michael/foody/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.michael.foody.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.michael.foody.data.database.entities.FavoritesEntity
 import com.michael.foody.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1,
     exportSchema = false
 )
