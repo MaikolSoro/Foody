99c4d5b2e8339973a26e3e140e89709eee1c5aec
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-01-05 13:05:04 -0600

    InstructionsFragment

--- app/src/main/java/com/michael/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/michael/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,17 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.michael.foody.R
+import com.michael.foody.databinding.FragmentInstructionsBinding
+import com.michael.foody.models.Result
+import com.michael.foody.util.Constants
 
 class InstructionsFragment : Fragment() {
+
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        binding.instructionsWebView.loadUrl(websiteUrl)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
