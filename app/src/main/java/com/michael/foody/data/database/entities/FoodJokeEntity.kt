4959ad3888ce450dfc069197a898b35fc78ba21d
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-01-18 19:19:08 -0600

    Offline Cache foodJoke

---
+++ app/src/main/java/com/michael/foody/data/database/entities/FoodJokeEntity.kt
@@ -0,0 +1,17 @@
+package com.michael.foody.data.database.entities
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.michael.foody.models.FoodJoke
+import com.michael.foody.util.Constants.Companion.FOOD_JOKE_TABLE
+
+@Entity(tableName = FOOD_JOKE_TABLE)
+class FoodJokeEntity (
+    @Embedded
+    var  foodJoke: FoodJoke
+
+){
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
