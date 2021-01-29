29cdcdcff5517f6a5d3c07a91b4034899f7f7fbc
Maikol Soro <maikolsoro.z1998@gmail.com>
2021-01-28 18:47:52 -0600

    Add splash screen

--- app/src/main/java/com/michael/foody/ui/MainActivity.kt
+++ app/src/main/java/com/michael/foody/ui/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setTheme(R.style.AppTheme)
         setContentView(binding.root)
